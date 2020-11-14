63fd2368f1709f41ade3e85568d1d3c1e5eab315
kkristof200 <kovacskristof200@gmail.com>
2020-11-14 20:46:52 +0200

    no message

--- demo.py
+++ demo.py
@@ -1,25 +1,17 @@
-# from simple_multiprocessing import MultiProcess, Task
-# import random, time
+from simple_multiprocessing import MultiProcess, Task
+import random, time
 
-# def test(i: int) -> float:
-#     print('started:', i)
-#     start = time.time()
+def test(i: int) -> float:
+    print('started:', i)
+    start = time.time()
 
-#     if random.random() < 0.5:
-#         while True:
-#             time.sleep(0.01)
+    if random.random() < 0.5:
+        while True:
+            time.sleep(0.01)
 
-#     return time.time() - start
+    return time.time() - start
 
-# tasks = [Task(test, i) for i in range(5)]
-# results = MultiProcess(tasks).solve(timeout=1)
+tasks = [Task(test, i) for i in range(5)]
+results = MultiProcess(tasks).solve(timeout=1)
 
-# [print(i, type(r), r) for i, r in enumerate(results)]
-import time
-
-async def f(i: int):
-    time.sleep(i)
-    print(i)
-
-f(4)
-f(5)
+[print(i, type(r), r) for i, r in enumerate(results)]
