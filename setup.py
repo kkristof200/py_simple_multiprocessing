4361d8f06085168a31e3450b0e31aab0302824ec
kkristof200 <kovacskristof200@gmail.com>
2020-11-14 19:15:03 +0200

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="simple_multiprocessing",
-    version="0.0.3",
+    version="0.0.4",
     author="Kristof",
     description="execute multiple async tasks as simple as possible",
     long_description=long_description,
--- simple_multiprocessing/multi_process.py
+++ simple_multiprocessing/multi_process.py
@@ -25,7 +25,7 @@ class MultiProcess:
 
         Args:
             tasks (List[Optional[Task]], optional): Tasks to innit with. More can be added later. Defaults to [].
-        """    
+        """
         self.tasks = tasks
 
 
